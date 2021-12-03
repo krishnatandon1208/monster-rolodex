1fa41e40b675f7ac7502d2b4225c1d5ee37db651
Krishna Tandon <krishnatandon.1208@gmail.com>
2021-12-04 01:57:15 +0530

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { CardList } from './components/card-list/card-list';
+import { SearchBox } from './components/search-box/search-box.component';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends Component {
 
-export default App;
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: ""
+    }
+
+    // this.handleChange = this.handleChange.bind(this);
+  }
+
+  // handleChange(e) {
+  //   //how to bind the keyword this on line 19 with the scope of this class ?
+  //   this.setState({ searchField: e.target.value });
+  // }
+
+  handleChange = (event) => {
+    this.setState({ searchField: event.target.value })
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }))
+      .catch(error => console.log(error));
+  }
+
+  render() {
+
+    const { monsters, searchField } = this.state;
+    const filterMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()) || monster.email.toLowerCase().includes(searchField.toLowerCase()));
+
+    return (
+      <div className="App">
+        <h1>Monster Rolodex</h1>
+        <SearchBox
+          placeholder="Search monsters"
+          // handleChange={this.handleChange.bind(this)}
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filterMonsters} />
+      </div>
+    )
+  }
+}
